c3924f98b254b699ca44a2ef1c18687fb49fb7d2
REWANTHWARSUNNNITH <rewanthwarsunnith7569@gmail.com>
2026-06-15 12:12:50 +0530

    fix database url

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-DATABASE_URL =os.getenv("postgresql://postgres:root@localhost:5432/tododb")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:root@localhost:5432/tododb")
+
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit = False,autoflush = False,bind = engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
